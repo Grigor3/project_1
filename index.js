00dff282b46bd31cdb4a798a32bebf9806d0bf7d
Grigor3 <65889385+Grigor3@users.noreply.github.com>
2023-10-05 13:14:27 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
 
 
-// function countText() {
-//     let text = document.getElementById("text-input").value;
+function countText() {
+    let text = document.getElementById("text-input").value;
 
-//     let letterCount = text.length;
-//     let wordCount = text.split(/\S+/).length - 1;
-//     let sentenceCount = text.split(/[.|!|?]+/).length - 1;
+    let letterCount = text.length;
+    let wordCount = text.split(/\S+/).length - 1;
+    let sentenceCount = text.split(/[.|!|?]+/).length - 1;
 
-//     let stats = "Количество букв: " + letterCount + " " +
-//         "Количество слов: " + wordCount + " " +
-//         "Количество предложений: " + sentenceCount;
-//     document.getElementById("text-stats").innerHTML = stats;
-//     document.getElementById("text-input").value = "";
-// }
-
-function solution(string) {
-    return (string.replace(/([A-Z])/g, ' $1'));
-
+    let stats = "Количество букв: " + letterCount + " " +
+        "Количество слов: " + wordCount + " " +
+        "Количество предложений: " + sentenceCount;
+    document.getElementById("text-stats").innerHTML = stats;
+    document.getElementById("text-input").value = "";
 }
-let arr = 'camelCasing'
-console.log(solution(arr))
 
 
